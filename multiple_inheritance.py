28091a4ccdf77ae26eb6086454e1b7520aa57412
lpaulino4543 <64877774+lpaulino4543@users.noreply.github.com>
2022-06-14 21:36:03 -0400

    Create multiple_inheritance.py

---
+++ multiple_inheritance.py
@@ -0,0 +1,25 @@
+class Prey:
+    def flee(self):
+        print("This animal flees")
+        
+class Predator:
+    def hunt(self):
+        print("This animal hunts")
+
+class Rabbit(Prey):
+    pass
+
+class Hawk(Predator):
+    pass
+
+class Fish(Prey, Predator):
+    pass
+
+rabbit = Rabbit()
+hawk = Hawk()
+fish = Fish()
+
+rabbit.flee()
+hawk.hunt()
+fish.hunt()
+fish.flee()
